9ea5808185fae7c1da3d1eb3e59779953f436463
Nathan Orick <cnathanorick@gmail.com>
2023-07-08 18:23:19 -0400

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 const routes: Routes = [
   { path: 'oauth2callback', component: AuthCallbackComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: '', redirectTo: '/transactions', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
-  declarations: [AppComponent, GoogleAuthComponent, AuthCallbackComponent],
+  declarations: [
+    AppComponent,
+    GoogleAuthComponent,
+    AuthCallbackComponent,
+    PageNotFoundComponent,
+    TransactionsComponent,
+    SettingsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
