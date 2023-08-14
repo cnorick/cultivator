34c4f0218ea3b81f6177c89cf928bbc1f5d0fcb7
Nathan Orick <cnathanorick@gmail.com>
2023-08-14 16:26:01 -0400

    hide about button if logged in

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { GoogleAuthService } from 'src/app/services/google-auth.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  constructor(private googleAuth: GoogleAuthService) {}
+
+  loggedIn$ = this.googleAuth.loggedIn$;
+}
