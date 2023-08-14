93f44e6542848f296e9c08a3caa77bcc06666e5f
Nathan Orick <cnathanorick@gmail.com>
2023-08-14 14:23:46 -0400

    move breadcrumb logic to service

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { filter, distinctUntilChanged } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { BreadcrumbsService } from 'src/app/services/breadcrumbs.service';
 
 export interface BreadCrumb {
   label: string;
@@ -12,73 +11,8 @@ export interface BreadCrumb {
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
 })
-export class BreadcrumbsComponent implements OnInit {
-  public breadcrumbs: BreadCrumb[];
+export class BreadcrumbsComponent {
+  constructor(private breadcrumbsService: BreadcrumbsService) {}
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-  }
-
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        distinctUntilChanged()
-      )
-      .subscribe(() => {
-        this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-      });
-  }
-
-  /**
-   * Recursively build breadcrumb according to activated route.
-   * @param route
-   * @param url
-   * @param breadcrumbs
-   */
-  buildBreadCrumb(
-    route: ActivatedRoute,
-    url: string = '',
-    breadcrumbs: BreadCrumb[] = []
-  ): BreadCrumb[] {
-    //If no routeConfig is avalailable we are on the root path
-    let label =
-      route.routeConfig && route.routeConfig.data
-        ? route.routeConfig.data['breadcrumb']
-        : '';
-    let isClickable =
-      route.routeConfig &&
-      route.routeConfig.data &&
-      route.routeConfig.data['isClickable'];
-    let path =
-      route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
-
-    // If the route is dynamic route such as ':id', remove it
-    const lastRoutePart = path!.split('/').pop();
-    const isDynamicRoute = lastRoutePart!.startsWith(':');
-    if (isDynamicRoute && !!route.snapshot) {
-      const paramName = lastRoutePart!.split(':')[1];
-      path = path!.replace(lastRoutePart!, route.snapshot.params[paramName]);
-      label = route.snapshot.params[paramName];
-    }
-
-    //In the routeConfig the complete path is not available,
-    //so we rebuild it each time
-    const nextUrl = path ? `${url}/${path}` : url;
-
-    const breadcrumb: BreadCrumb = {
-      label: label,
-      url: nextUrl,
-    };
-    // Only adding route with non-empty label
-    const newBreadcrumbs = breadcrumb.label
-      ? [...breadcrumbs, breadcrumb]
-      : [...breadcrumbs];
-    if (route.firstChild) {
-      //If we are not on our current path yet,
-      //there will be more children to look after, to build our breadcumb
-      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
-    }
-    return newBreadcrumbs;
-  }
+  breadcrumbs$ = this.breadcrumbsService.breadcrumbs$;
 }
