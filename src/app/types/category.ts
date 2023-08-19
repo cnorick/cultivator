decc8cc2d6019c8583f1f1abf19b2c216680787f
Nathan Orick <cnathanorick@gmail.com>
2023-08-19 11:31:27 -0400

    Make the secret string more readable

--- src/app/types/category.ts
+++ src/app/types/category.ts
@@ -1,5 +1,4 @@
-export const CATEGORY_LOADING_VAL =
-  '<secret loading string with special characters so that no one puts them in a form (⠀⠀⠀)>';
+export const CATEGORY_LOADING_VAL = 'Loading.............⠀⠀⠀';
 export interface Category {
   category: string;
   group: string;
